fc0730379d024fbfcf0b8614de7223fb29792f17
Ali Burak <alibarac41@icloud.com>
2023-11-24 00:49:56 +0300

    Initial Commit

---
+++ kratoTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kratoTask
+//
+//  Created by Ali Burak Baraç on 24.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
